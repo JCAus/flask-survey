aba15cb1dbe4ba7957d76e713ae39168d9df92d1
Jimmy Austin <jimmycaustin@gmail.com>
2022-08-29 00:01:03 -0400

    with flask sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -8,6 +8,8 @@ app.config['SECRET_KEY'] = "chickens-are-cool"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
+RESPONSES = "responses"
+
 QUESTIONS = {
     0: satisfaction_survey.questions[0].question,
     1: satisfaction_survey.questions[1].question,
@@ -16,24 +18,31 @@ QUESTIONS = {
 }
 
 
-RESPONSES = []
-
 @app.route('/')
 def show_title_page():   
     title = satisfaction_survey.title
     info = satisfaction_survey.instructions
+    
     return render_template("begin-survey.html", title=title, info=info)
+
+@app.route('/list', methods=["POST"])
+def make_list():
+    session[RESPONSES] = []
+    return redirect("/questions/0")
 
 @app.route('/questions/<int:id>')
 def show_questions(id):
-    if id == len(RESPONSES):
+    responses = session.get(RESPONSES)
+    
+    if id == len(responses):
         question = QUESTIONS[id]
         choice_1 = satisfaction_survey.questions[id].choices[0]
         choice_2 = satisfaction_survey.questions[id].choices[1]
         return render_template("questions.html", question=question, choice_1=choice_1, choice_2=choice_2)
-    if id > len(RESPONSES) or id < len(RESPONSES):
+    
+    if id > len(responses) or id < len(responses):
         flash("Invalid URL! You've been redirected back to the survey.")
-        return redirect(f'/questions/{len(RESPONSES)}')
+        return redirect(f'/questions/{len(responses)}')
     
     
 
@@ -41,12 +50,14 @@ def show_questions(id):
 def save_answer():
     
     answer = request.form["choices"]
-    RESPONSES.append(answer)
+    responses = session[RESPONSES]
+    responses.append(answer)
+    session[RESPONSES] = responses
     
-    if len(RESPONSES) == len(QUESTIONS):
+    if len(responses) == len(QUESTIONS):
         return redirect('/completed')
-    if len(RESPONSES) != len(QUESTIONS):
-        return redirect(f'/questions/{len(RESPONSES)}')
+    if len(responses) != len(QUESTIONS):
+        return redirect(f'/questions/{len(responses)}')
 
 @app.route('/completed')
 def say_thanks():
